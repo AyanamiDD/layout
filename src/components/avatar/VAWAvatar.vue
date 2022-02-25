b819454b1e4df65a2256cd2d6005336ecee75f00
AyanamiDD <84127424+AyanamiDD@users.noreply.github.com>
2022-02-25 09:49:01 +0800

    Update VAWAvatar.vue

--- src/components/avatar/VAWAvatar.vue
+++ src/components/avatar/VAWAvatar.vue
@@ -37,7 +37,7 @@ export default {
   },
   created(){
     //从vuex里获取姓名和头像数据
-    this.name = store_.state.user.userId
+    this.name = store_.state.user.userName
     this.avatarSrc=store_.state.user.avatar
   },
   methods: {
